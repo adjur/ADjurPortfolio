8d2a87cc2f84a4cda2d49201ae8e0563ee21eb75
Andi Dj <155870242+adjur@users.noreply.github.com>
2024-06-11 11:28:31 -0700

    title updates

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,7 +7,7 @@ export const Hero = () => {
     return (
         <section className={styles.container}>
             <div className={styles.content}>
-                <h1 className='heroTitle'>Hi, I'm Andi</h1>
+                <h1 className={styles.title}>Hi, I'm Andi</h1>
                 <p className={styles.description}>
                     I am a front-end developer skilled in React and Node.js, committed to crafting responsive and dynamic web applications. Reach out if you'd like to learn more!</p>
                 <a className={styles.contactBtn} href="mailto:a.djur_12@hotmail.com">
